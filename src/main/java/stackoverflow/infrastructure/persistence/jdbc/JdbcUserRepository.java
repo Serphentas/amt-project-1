898de60248b61ee1048bc74e2211a9909c1338df
scout407 <stephane.bouyiatiotis@heig-vd.ch>
2020-10-08 16:38:22 +0200

    ajout d'une méthode pour créer un utilisateur

--- src/main/java/stackoverflow/infrastructure/persistence/jdbc/JdbcUserRepository.java
+++ src/main/java/stackoverflow/infrastructure/persistence/jdbc/JdbcUserRepository.java
@@ -1,9 +1,14 @@
 package stackoverflow.infrastructure.persistence.jdbc;
 
+import stackoverflow.domain.person.Person;
+
 import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @ApplicationScoped
 @Named("JdbcUserRepository")
@@ -19,4 +24,20 @@ public class JdbcUserRepository {
         this.dataSource = dataSource;
     }
 
+    public ResultSet addUser(Person person){
+        try {
+            PreparedStatement statement = dataSource.getConnection().prepareStatement(
+                    "INSERT INTO User(pseudo, name, surname, email, password) VALUES ?, ?, ?, ?, ?");
+            statement.setString(1, person.getUsername());
+            statement.setString(2, person.getFirstName());
+            statement.setString(3, person.getLastName());
+            statement.setString(4, person.getEmail());
+            statement.setString(5, person.getEncryptedPassword());
+
+            return statement.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
 }
