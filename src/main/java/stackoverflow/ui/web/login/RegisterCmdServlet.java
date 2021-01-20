e580cd436ccab56daadcfda39280e3cc2446f920
Serphentas <Serphentas@users.noreply.github.com>
2021-01-20 01:36:54 +0100

    initial gamification trial

--- src/main/java/stackoverflow/ui/web/login/RegisterCmdServlet.java
+++ src/main/java/stackoverflow/ui/web/login/RegisterCmdServlet.java
@@ -4,6 +4,7 @@ import stackoverflow.application.ServiceReg;
 import stackoverflow.application.identitymngmt.login.ProposeRegisterCmd;
 import stackoverflow.application.identitymngmt.login.RegistrationFailedException;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 @WebServlet(name = "RegisterCmdServlet", urlPatterns = "/register.do")
@@ -19,9 +23,16 @@ public class RegisterCmdServlet extends HttpServlet {
     @Inject
     ServiceReg serviceReg;
 
+    @Resource(lookup = "gamification/badges")
+    String gamificationURL;
+    @Resource(lookup = "gamification/apikey")
+    String gamificationAPIKey;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getSession().getAttribute("errors");
+
+        System.out.println(gamificationURL);
 
         ProposeRegisterCmd proposeRegisterCmd = ProposeRegisterCmd.builder()
             .username(req.getParameter("username"))
@@ -34,6 +45,24 @@ public class RegisterCmdServlet extends HttpServlet {
 
         try {
             serviceReg.getIdentityMngmtFacade().register(proposeRegisterCmd);
+
+            URL url = new URL(gamificationURL);
+            String data = String.format(
+                "{" +
+                    "\"%s\": \"%s\"" +
+                "}",
+                "asd", "123"
+            );
+            HttpURLConnection postConnection = (HttpURLConnection) url.openConnection();
+            postConnection.setRequestMethod("POST");
+            postConnection.setRequestProperty("x-api-key", gamificationAPIKey);
+            postConnection.setRequestProperty("Content-Type", "application/json");
+            postConnection.setDoOutput(true);
+            OutputStream os = postConnection.getOutputStream();
+            os.write(data.getBytes());
+            os.flush();
+            os.close();
+
             req.getRequestDispatcher("/login.do").forward(req, resp);
             return;
         } catch(RegistrationFailedException e) {
